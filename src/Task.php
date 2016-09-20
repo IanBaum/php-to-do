e5136261dbe9eed320aafb16fdf66a0d0f9768bd
Ian Baum and Rebecca Allen <rebecca@>
2016-09-20 14:46:28 -0400

    add in category class and tests, home twig

--- src/Task.php
+++ src/Task.php
@@ -2,15 +2,22 @@
     class Task
     {
         private $description;
+        private $id;
 
-        function __construct($description)
+        function __construct($description, $id = null)
         {
             $this->description = $description;
+            $this->id = $id;
         }
 
         function setDescription($new_description)
         {
             $this->description = (string) $new_description;
+        }
+
+        function getId()
+        {
+            return $this->id;
         }
 
         function getDescription()
@@ -21,6 +28,7 @@
         function save()
         {
             $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
@@ -30,7 +38,8 @@
             foreach($returned_tasks as $task)
             {
                 $description = $task['description'];
-                $new_task = new Task($description);
+                $id= $task['id'];
+                $new_task = new Task($description, $id);
                 array_push($tasks, $new_task);
             }
             return $tasks;
@@ -39,6 +48,19 @@
         static function deleteAll()
         {
             $GLOBALS['DB']->exec("DELETE FROM tasks;");
+        }
+
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task){
+                $task_id = $task->getId();
+                if($task_id == $search_id){
+                  $found_task = $task;
+                }
+            }
+            return $found_task;
         }
     }
 ?>
