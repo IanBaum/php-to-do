eb5327f6c9c7a3cdf3967360e1aa6d7976beb433
Ian Baum and Rebecca Allen <rebecca@>
2016-09-20 11:02:26 -0400

    set up all the directories and files, finish composer.json, .gitignore, index.php, task.php, app.php

---
+++ app/app.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO ($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'         => __DIR__.'/../views'
+    ));
+
+  $app->get("/", function() use ($app){
+      return $app['twig']->render('home.html.twig', array('tasks'=>Task::getALL()));
+  });
+
+  $app->post("tasks", funtion() use($app){
+      $task = new Task($_POST['decription']);
+      $task->save();
+      return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+  });
+
+  $app->post("delete_tasks", function() use ($app){
+      Task::deleteAll();
+      return $app['twig']->render('delete_tasks.html.twg');
+  });
+
+  return $app;
+?>
