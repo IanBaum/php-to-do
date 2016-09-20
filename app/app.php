eb1a0b385b1ca6e7715e7841f8eb877d4288519f
Ian Baum and Rebecca Allen <ian@>
2016-09-20 15:00:17 -0400

    fix the routes in app.php

--- app/app.php
+++ app/app.php
@@ -1,6 +1,8 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
+    date_default_timezone_set("America/New_York");
 
     $app = new Silex\Application();
 
@@ -13,18 +15,26 @@
     ));
 
   $app->get("/", function() use ($app){
-      return $app['twig']->render('home.html.twig', array('tasks'=>Task::getALL()));
+      return $app['twig']->render('index.html.twig');
   });
 
-  $app->post("tasks", funtion() use($app){
+  $app->get("/tasks", function() use ($app){
+      return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+  });
+
+  $app->get("/categories", function() use ($app){
+      return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+  });
+
+  $app->post("/tasks", function() use($app){
       $task = new Task($_POST['decription']);
       $task->save();
-      return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+      return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
   });
 
-  $app->post("delete_tasks", function() use ($app){
+  $app->post("/delete_tasks", function() use ($app){
       Task::deleteAll();
-      return $app['twig']->render('delete_tasks.html.twg');
+      return $app['twig']->render('index.html.twg');
   });
 
   return $app;
